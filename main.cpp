9f8032af5092f2afe73d7b8645d93d2e70be1ca2
Keegan Lawley <keeganlk.7@gmail.com>
2021-01-28 17:45:33 -0600

    adding the laplacian from the tutorial

--- main.cpp
+++ main.cpp
@@ -1,34 +1,105 @@
+//   igl::readOFF("../data/bunny.off", V, F);
+#include <igl/barycenter.h>
+#include <igl/cotmatrix.h>
+#include <igl/doublearea.h>
+#include <igl/grad.h>
+#include <igl/jet.h>
+#include <igl/massmatrix.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/readDMAT.h>
+#include <igl/readOFF.h>
+#include <igl/repdiag.h>
 #include <igl/opengl/glfw/Viewer.h>
+
+#include <iostream>
+
+Eigen::MatrixXd V,U;
+Eigen::MatrixXi F;
+Eigen::SparseMatrix<double> L;
+igl::opengl::glfw::Viewer viewer;
 
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+  using namespace Eigen;
+  using namespace std;
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+  // Load a mesh in OFF format
+  igl::readOFF("../data/bunny.off", V, F);
+
+  // Compute Laplace-Beltrami operator: #V by #V
+  igl::cotmatrix(V,F,L);
+
+  // Alternative construction of same Laplacian
+  SparseMatrix<double> G,K;
+  // Gradient/Divergence
+  igl::grad(V,F,G);
+  // Diagonal per-triangle "mass matrix"
+  VectorXd dblA;
+  igl::doublearea(V,F,dblA);
+  // Place areas along diagonal #dim times
+  const auto & T = 1.*(dblA.replicate(3,1)*0.5).asDiagonal();
+  // Laplacian K built as discrete divergence of gradient or equivalently
+  // discrete Dirichelet energy Hessian
+  K = -G.transpose() * T * G;
+  cout<<"|K-L|: "<<(K-L).norm()<<endl;
+
+  const auto &key_down = [](igl::opengl::glfw::Viewer &viewer,unsigned char key,int mod)->bool
+  {
+    switch(key)
+    {
+      case 'r':
+      case 'R':
+        U = V;
+        break;
+      case ' ':
+      {
+        // Recompute just mass matrix on each step
+        SparseMatrix<double> M;
+        igl::massmatrix(U,F,igl::MASSMATRIX_TYPE_BARYCENTRIC,M);
+        // Solve (M-delta*L) U = M*U
+        const auto & S = (M - 0.001*L);
+        Eigen::SimplicialLLT<Eigen::SparseMatrix<double > > solver(S);
+        assert(solver.info() == Eigen::Success);
+        U = solver.solve(M*U).eval();
+        // Compute centroid and subtract (also important for numerics)
+        VectorXd dblA;
+        igl::doublearea(U,F,dblA);
+        double area = 0.5*dblA.sum();
+        MatrixXd BC;
+        igl::barycenter(U,F,BC);
+        RowVector3d centroid(0,0,0);
+        for(int i = 0;i<BC.rows();i++)
+        {
+          centroid += 0.5*dblA(i)/area*BC.row(i);
+        }
+        U.rowwise() -= centroid;
+        // Normalize to unit surface area (important for numerics)
+        U.array() /= sqrt(area);
+        break;
+      }
+      default:
+        return false;
+    }
+    // Send new positions, update normals, recenter
+    viewer.data().set_vertices(U);
+    viewer.data().compute_normals();
+    viewer.core().align_camera_center(U,F);
+    return true;
+  };
+
+
+  // Use original normals as pseudo-colors
+  MatrixXd N;
+  igl::per_vertex_normals(V,F,N);
+  MatrixXd C = N.rowwise().normalized().array()*0.5+0.5;
+
+  // Initialize smoothing with base mesh
+  U = V;
+  viewer.data().set_mesh(U, F);
+  viewer.data().set_colors(C);
+  viewer.callback_key_down = key_down;
+
+  cout<<"Press [space] to smooth."<<endl;;
+  cout<<"Press [r] to reset."<<endl;;
+  return viewer.launch();
 }
